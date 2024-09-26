89bcec81098c179a96ce8705ca1b38d82dbd9108
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2024-09-26 10:50:00 +0200

    Update dependencies

--- src/capture/combinePdfs.ts
+++ src/capture/combinePdfs.ts
@@ -5,7 +5,7 @@ export async function combinePdfs(pdfs: RArr<Buffer>): Promise<Buffer> {
   return await orThrowAsync(async () => {
     const merger = new PDFMerger();
     for (const pdf of pdfs) {
-      await merger.add(pdf, '1');
+      await merger.add(pdf.buffer, '1');
     }
     return await merger.saveAsBuffer();
   }, 'Failed to merge PDFs');
